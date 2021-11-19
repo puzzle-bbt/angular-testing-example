4a19941cde03f394611ac68379a638f35aeffcbb
Lias <kleisa@puzzle.ch>
2021-11-19 13:33:40 +0100

    Add e2e tests

--- cypress/integration/change-hero.spec.ts
+++ cypress/integration/change-hero.spec.ts
@@ -3,6 +3,7 @@ describe('change a hero', () => {
   beforeEach(() => {
   });
 
+
   it('click top hero, change, save', () => {
     cy.visit('/');
 
@@ -27,6 +28,56 @@ describe('change a hero', () => {
     cy.get('button').contains('go back').click();
 
     cy.get('.heroes-menu > a').first().should('contain', 'Narco');
+  });
+
+
+  it('search hero and click on it', () => {
+    cy.visit('/');
+
+    cy.get('#search-box').type("Bombas");
+    cy.wait(400);
+    cy.get(".search-result > li").first().click();
+
+    var heroLink = cy.get('#hero-name');
+    heroLink.should('have.value', 'Bombasto');
+    heroLink.clear().type('Bombasto 2');
+    cy.get('button').contains('save').click();
+
+    cy.get('#search-box').type("Bombasto 2");
+    cy.wait(400);
+    cy.get(".search-result > li").first().should("contain", "Bombasto 2");
+  });
+
+
+  it('remove messages', () => {
+    cy.visit('/');
+
+    cy.get('.heroes-menu > a').eq(2).should('contain', 'Celeritas');
+    cy.get('.heroes-menu > a').eq(2).click();
+
+    var heroLink = cy.get('#hero-name');
+    heroLink.should('have.value', 'Celeritas');
+    heroLink.clear().type('Test Name');
+    cy.get('button').contains('save').click();
+
+    cy.get('.clear').click();
+
+
+    cy.get('.messagesDiv').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+
+
+  });
+
+
+  it('remove hero', () => {
+    cy.visit('/');
+
+    cy.get('nav > a').eq(1).should("contain", "Heroes").click();
+
+    cy.get('.heroes > li').eq(3).should('contain', 'Celeritas').find("button").click();
+
+    cy.get('.heroes').find('li').should('have.length', 9);
+
   });
 
 })
